4300498dcafbd499f3412fb87ea1b7c7eab8961b
Ovidiu Golban <68772875+OviGolban@users.noreply.github.com>
2022-03-18 14:07:01 +0200

    Delete Lab1 directory

--- Lab1/curs2.pl
+++
@@ -1,3 +0,0 @@
-member1(X, [X|_]).
-member1(X, [_|T]):- member1(X,T).
-member1(_, []):- fail.
--- Lab1/exemplu.pl
+++
@@ -1,5 +0,0 @@
-suntem_in_sala_108.
-animal(elefant).
-animal('antilopa Gnu'). 
-inaltime(girafa, 5.5). % înălțimea îi în metrii
-arbore( t(1, t(-2, nil, nil), t(8,nil,nil)) ).
